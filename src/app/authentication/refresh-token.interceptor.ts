e52362f2da1117152d8d6636eda079ff1c41a957
Teddy Grandsire <teddy@grandsire.org>
2018-05-06 12:39:48 +0200

    Debug for refreshin token

--- src/app/authentication/refresh-token.interceptor.ts
+++ src/app/authentication/refresh-token.interceptor.ts
@@ -12,9 +12,14 @@ export class RefreshTokenInterceptor implements HttpInterceptor {
   ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(this.authenticationService.getRefreshToken());
+    console.log(this.authenticationService.getToken());
+    console.log(this.authenticationService.isAuthenticated());
+    console.log(request.url !== this.authenticationService.getRefreshTokenUrl());
+
     if (this.authenticationService.getRefreshToken() &&
     		this.authenticationService.getToken() &&
-    		! this.authenticationService.isAuthenticated &&
+    		! this.authenticationService.isAuthenticated() &&
     		request.url !== this.authenticationService.getRefreshTokenUrl()
     ) {
       this.authenticationService.refreshToken().subscribe();
